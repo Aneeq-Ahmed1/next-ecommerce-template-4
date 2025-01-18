ba4d17e3281a39772c26ce9aa68e3fa9cc849de2
Aneeq-Ahmed1 <aneeq.edward@outlook.com>
2025-01-18 21:17:46 +0500

    fething data in sanit

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import data from '@/data/chair.json';
+import data from "@/data/chair.json";
 
 export async function GET() {
     return NextResponse.json(data);
